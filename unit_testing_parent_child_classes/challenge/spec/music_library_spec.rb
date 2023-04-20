a6e53f01aaa3411f6b446cd83cde2df743be412d
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-20 16:37:26 +0100

    mock test for music_library.rb running GREEN

---
+++ unit_testing_parent_child_classes/challenge/spec/music_library_spec.rb
@@ -0,0 +1,12 @@
+require "music_library"
+
+RSpec.describe MusicLibrary do
+  it "adds and lists tracks" do
+    music_library = MusicLibrary.new
+    track_1 = double :track
+    track_2 = double :track
+    music_library.add(track_1)
+    music_library.add(track_2)
+    expect(music_library.all).to eq [track_1, track_2]
+  end
+end
