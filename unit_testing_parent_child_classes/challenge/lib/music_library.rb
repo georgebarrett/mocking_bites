4b0aac53871d3f884ded3507b4e3ed1fc9d509b7
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-20 17:02:37 +0100

    'search' method in music_library.rb constructed. relevant test running GREEN

--- unit_testing_parent_child_classes/challenge/lib/music_library.rb
+++ unit_testing_parent_child_classes/challenge/lib/music_library.rb
@@ -11,7 +11,9 @@ class MusicLibrary
     return @tracks
   end
   
-  def search(keyword) # keyword is a string
-    # Returns a list of tracks that match the keyword
+  def search(keyword)
+    return @tracks.select do |track|
+      track.matches?(keyword)
+    end
   end
 end
