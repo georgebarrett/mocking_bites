11e7e6db706ea7c690ee3e300e1de84418d1fa32
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-20 17:22:32 +0100

    'initialize' and 'matches?' methods adjusted. returns true when keyword matches an artist name. relevant test made in track_spec.rbrunning GREEN

--- unit_testing_parent_child_classes/challenge/lib/track.rb
+++ unit_testing_parent_child_classes/challenge/lib/track.rb
@@ -1,9 +1,11 @@
 class Track
   def initialize(title, artist)
     @title = title
+    @artist = artist
   end
 
   def matches?(keyword) 
     return true if @title.include? keyword
+    return true if @artist.include? keyword
   end
 end
--- unit_testing_parent_child_classes/challenge/spec/track_spec.rb
+++ unit_testing_parent_child_classes/challenge/spec/track_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Track do
     track = Track.new("one MATCHES two", "three four")
     expect(track.matches?("MATCHES")).to eq true
   end
+
+  it "matches when given a keyword in the artist" do
+    track = Track.new("one two", "three MATCHES four")
+    expect(track.matches?("MATCHES")).to eq true
+  end
+
 end
