a817d0a491ce766bce445209dcae855b47dfa19b
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-21 10:40:42 +0100

    challenege complete. mock tests constructed. all running GREEN

--- unit_testing_parent_child_classes/exercise/spec/task_list_spec.rb
+++ unit_testing_parent_child_classes/exercise/spec/task_list_spec.rb
@@ -10,3 +10,30 @@ RSpec.describe TaskList do
     task_list = TaskList.new
     expect(task_list.all_complete?).to eq false
   end
+
+  it "returns the list of tasks" do
+    task_list = TaskList.new
+    task = double :task
+    task_list.add(task)
+
+    expect(task_list.all).to eq [task]
+  end
+
+  it "returns true if all tasks are complete" do
+    task_list = TaskList.new
+    task_1 = double(:task_double, complete?: true)
+    task_list.add(task_1)
+
+    expect(task_list.all_complete?).to eq true
+  end
+
+  it "returns false if not all of the tasks are complete" do
+    task_list = TaskList.new
+    task_1 = double(:task_double, complete?: true)
+    task_2 = double(:task_double_2, complete?: false)
+    task_list.add(task_1)
+    task_list.add(task_2)
+
+    expect(task_list.all_complete?).to eq false
+  end
+end
