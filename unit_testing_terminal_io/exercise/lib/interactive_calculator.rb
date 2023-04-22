8d1655dfb556baf0b4bcdf652d8b26c55a4d2c2e
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-22 15:30:19 +0100

    fail message occurs if user enters a string rather than a number for the first number entry. mock test running GREEN

--- unit_testing_terminal_io/exercise/lib/interactive_calculator.rb
+++ unit_testing_terminal_io/exercise/lib/interactive_calculator.rb
@@ -10,11 +10,19 @@ class InteractiveCalculator
   def run
     @terminal.puts "Hello. I will subtract two numbers."
     @terminal.puts "Please enter a number"
-    number_1 = @terminal.gets.to_i
+    number_1 = get_number
     @terminal.puts "Please enter another number"
     number_2 = @terminal.gets.to_i
     @terminal.puts "Here is your result:"
     @terminal.puts "#{number_1} - #{number_2} = #{number_1 - number_2}"
+  end
+
+  private
+
+  def get_number
+    response = @terminal.gets
+    return response.to_i if response.to_i.to_s == response
+    fail "Please enter numbers only."
   end
 
 end
--- unit_testing_terminal_io/exercise/spec/interactive_calculator_spec.rb
+++ unit_testing_terminal_io/exercise/spec/interactive_calculator_spec.rb
@@ -15,4 +15,15 @@ RSpec.describe InteractiveCalculator do
     interactive_calculator = InteractiveCalculator.new(terminal)
     interactive_calculator.run
   end
+
+  it "fails if string is given rather than an integer" do
+    terminal = double :terminal
+    
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("Autechre").ordered
+
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    expect { interactive_calculator.run }.to raise_error "Please enter numbers only."
+  end
 end
