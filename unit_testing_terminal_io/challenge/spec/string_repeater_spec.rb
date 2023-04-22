2720b7cc5a945b57859cfd116bdd4fc1d5bea7dd
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-22 16:34:34 +0100

    refactor

--- unit_testing_terminal_io/challenge/spec/string_repeater_spec.rb
+++ unit_testing_terminal_io/challenge/spec/string_repeater_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe StringRepeater do
 
     expect(io).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
     expect(io).to receive(:puts).with("Please enter a string").ordered
-    expect(io).to receive(:gets).and_return("TWIX").ordered
+    expect(io).to receive(:gets).and_return("AphexTwin").ordered
     expect(io).to receive(:puts).with("Please enter the number of repeats").ordered
-    expect(io).to receive(:gets).and_return("10").ordered
+    expect(io).to receive(:gets).and_return("2").ordered
     expect(io).to receive(:puts).with("Here is your result:").ordered
-    expect(io).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX").ordered
+    expect(io).to receive(:puts).with("AphexTwinAphexTwin").ordered
     
     string_repeater = StringRepeater.new(io)
     string_repeater.run
