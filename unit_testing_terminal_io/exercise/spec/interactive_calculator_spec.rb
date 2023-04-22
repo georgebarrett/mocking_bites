5f16545c8e791319fd1db780ff4165d7136ecf04
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-22 15:18:50 +0100

    interactive_calculator.rb constructed. mock test in interactive_calculator_spec.rb running GREEN

---
+++ unit_testing_terminal_io/exercise/spec/interactive_calculator_spec.rb
@@ -0,0 +1,18 @@
+require "interactive_calculator"
+
+RSpec.describe InteractiveCalculator do
+  it "subtracts two numbers using terminal" do
+    terminal = double :terminal
+    
+    expect(terminal).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(terminal).to receive(:puts).with("Please enter a number").ordered
+    expect(terminal).to receive(:gets).and_return("45").ordered
+    expect(terminal).to receive(:puts).with("Please enter another number").ordered
+    expect(terminal).to receive(:gets).and_return("3").ordered
+    expect(terminal).to receive(:puts).with("Here is your result:").ordered
+    expect(terminal).to receive(:puts).with("45 - 3 = 42").ordered
+
+    interactive_calculator = InteractiveCalculator.new(terminal)
+    interactive_calculator.run
+  end
+end
