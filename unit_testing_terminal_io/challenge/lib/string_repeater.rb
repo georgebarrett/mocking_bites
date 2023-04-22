fc34c63bc173e871471ada63f4c54c988e7a36a5
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-22 16:33:05 +0100

    string_repeater.rb complete. mock test in string_repeater_spec.rb running GREEN

--- unit_testing_terminal_io/challenge/lib/string_repeater.rb
+++ unit_testing_terminal_io/challenge/lib/string_repeater.rb
@@ -1,10 +1,17 @@
-string_repeater = StringRepeater.new(Kernel)
-string_repeater.run
+class StringRepeater
 
-# Hello. I will repeat a string many times.
-# Please enter a string
-# TWIX
-# Please enter a number of repeats
-# 10
-# Here is your result:
-# TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX
+  def initialize(io = Kernel)
+    @io = io
+  end
+  
+  def run
+    @io.puts "Hello. I will repeat a string many times."
+    @io.puts "Please enter a string"
+    string = @io.gets.chomp
+    @io.puts "Please enter the number of repeats"
+    repeats = @io.gets.to_i
+    result = (string * repeats)
+    @io.puts "Here is your result:"
+    @io.puts result
+  end
+end
---
+++ unit_testing_terminal_io/challenge/spec/string_repeater_spec.rb
@@ -0,0 +1,18 @@
+require "string_repeater"
+
+RSpec.describe StringRepeater do
+  it "repeats a string as many times as the user likes" do 
+    io = double(:io)
+
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(io).to receive(:puts).with("Please enter a string").ordered
+    expect(io).to receive(:gets).and_return("TWIX").ordered
+    expect(io).to receive(:puts).with("Please enter the number of repeats").ordered
+    expect(io).to receive(:gets).and_return("10").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX").ordered
+    
+    string_repeater = StringRepeater.new(io)
+    string_repeater.run
+  end
+end
